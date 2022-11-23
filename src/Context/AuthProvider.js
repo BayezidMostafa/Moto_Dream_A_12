0fa20452ca4f0cfb311f2c216c745c65eaf5d55a
Bayezid Mostafa <hiperx1@gmail.com>
2022-11-24 00:41:10 +0600

    Authentication Process Added in AuthContext

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import {getAuth} from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateCurrentUser} from 'firebase/auth'
 import app from '../Firebase/Firebase.config';
 
 const auth = getAuth(app);
@@ -7,9 +7,49 @@ export const AuthContext = createContext()
 
 
 const AuthProvider = ({children}) => {
-    
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const googleProvider = new GoogleAuthProvider();
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const googleProviderLogin = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider);
+    }
+    const logInUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    const userLogOut = () => {
+        return signOut(auth);
+    }
+    const updateUserInfo = profile => {
+        setLoading(true)
+        return updateCurrentUser(auth.currentUser, profile)
+    }
 
-    const authInformation = {}
+    useEffect(()=> {
+        const unsubscribe = onAuthStateChanged(auth.currentUser, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    const authInformation = {
+        setLoading,
+        loading,
+        createUser,
+        updateUserInfo,
+        logInUser,
+        userLogOut,
+        googleProviderLogin,
+        user
+    }
 
     return (
         <AuthContext.Provider value={authInformation}>
